de1170741aff680f6662cc6351cdbc82bb4fd36c
luisluft <luberluft@gmail.com>
2021-09-02 10:44:00 -0400

    add toggle background buttons functionality

---
+++ script.js
@@ -0,0 +1,23 @@
+const { body } = document;
+
+function changeBackground(number) {
+  // save previous background
+  let previousBackground;
+  if (body.className) previousBackground = body.className;
+
+  // Reset styles
+  body.className = "";
+  switch (number) {
+    case "1":
+      return previousBackground === "background-1" ? false : body.classList.add("background-1");
+      break;
+    case "2":
+      return previousBackground === "background-2" ? false : body.classList.add("background-2");
+      break;
+    case "3":
+      return previousBackground === "background-3" ? false : body.classList.add("background-3");
+      break;
+    default:
+      break;
+  }
+}
